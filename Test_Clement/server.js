0b9a750b89e7a94e01c313862ddb6686c72e2a2a
ckli76 <ckli@hawaii.edu>
2019-12-12 01:04:32 -1000

    Assignment 2 & Lab15 Update

--- Test_Clement/server.js
+++ Test_Clement/server.js
@@ -1,10 +1,15 @@
+// Forked & Adapted from Clement Li: Assingment 2 - server.js (Login & Registration) -->
+
 var fs = require('fs');
 var express = require('express');
 var app = express();
 var myParser = require("body-parser");
 
 app.use(myParser.urlencoded({ extended: true }));
-var filename = 'user_data.json' // Set variable filename to reference user_data.json
+var filename = 'user_datatest.json' // Set variable filename to reference user_data.json
+
+//Assignment 2 Code
+var filename = 'user_datatest.json' // Set variable filename to reference user_data.json
 
 if (fs.existsSync(filename)) { //check to see if file exists
     stats = fs.statSync(filename);
@@ -16,107 +21,48 @@ if (fs.existsSync(filename)) { //check to see if file exists
     console.log(filename + ' does not exist!');
 }
 
-app.post("/login", function (request, response) {
-    loginData = request.body;
-    //console.log(loginData);
-    // Process login form POST and redirect to logged in page if ok, back to login page if not
-    the_username = loginData.username;
-    the_password = loginData.password;
-    //console.log(the_username)
-    //console.log(the_password)
-    if (typeof users_reg_data[the_username] != 'undefined') { //check if the username exists in the json data
-        if (users_reg_data[the_username].password == the_password)
-            response.send(the_username + ' logged in! ');
-    } else {
-        response.redirect('login.html');
-    }
-}
-);
+//Card Registration Code - Clement Li
 
-app.post("/register", function (request, response) {
-    regData = request.body;
-    console.log("Got the registration request");
-    console.log(request.body);
-    // process a simple register form
+app.post("/card_registered", function (request, response) {
+    cardData = request.body; //card data is set as variable
+    console.log("Got the card registration request"); //Lets admin know grabbing the registration data was a success
+    console.log(request.body); //Lets admin see what was inputted in all the fields
+    // process a card request
 
-    validerrors = false;
 
-    username_input = regData.username.toLowerCase();
-    password_input = regData.password;
-    REpassword_input = regData.repeat_password;
-    email_input = regData.email;
+    username_data = cardData.username;
+    title_data = cardData.title;
+    event_data = cardData.event;
+    note_data = cardData.note
+    date_data = cardData.date;
+    time_data = cardData.time;
+    description_data = cardData.description;
+    tag_data = cardData.tag
 
-    //Validate username
-    var letters = /^[0-9a-zA-Z]+$/;
+    //Figure out what username, figure out what tag. Than input the data into the JSON
 
-    if (typeof users_reg_data[username_input] != 'undefined') {
-        validerrors = true
-    };
-    if (username_input.length < 1 && username_input.length > -1) {
-        validerrors = true
-    };
-    if (username_input.length > 0 && username_input.length < 4) {
-        validerrors = true
-    };
-    if (username_input.length > 15) {
-        validerrors = true
-    };
-    if (username_input.match(letters)) { }
-    else {
-        validerrors = true
-    };
-
-    //Validate Password
-
-    if (password_input.length < 1 && password_input.length > -1) {
-        validerrors = true
-    };
-    if (password_input.length > 0 && password_input.length < 5) {
-        validerrors = true
-    };
-
-    //Validate Re-Entered Password
-
-    if (REpassword_input.length < 1 && REpassword_input.length > -1) {
-        validerrors = true
-    };
-    if (password_input != REpassword_input) {
-        validerrors = true
-    };
-
-    //Validate Email
-
-    var email_letters = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-
-    if (email_input.length < 1 && email_input.length > -1) {
-        validerrors = true
-    };
-
-    if (email_input.match(email_letters)) { }
-    else { validerrors = true };
-
-    username_input = regData.username;
-
-    if (validerrors == false) {
-        users_reg_data[username_input] = {};
-        users_reg_data[username_input].name = username_input;
-        users_reg_data[username_input].password = regData.password;
-        users_reg_data[username_input].email = regData.email;
-
-        console.log(username_input)
-
-        var output_data = JSON.stringify(users_reg_data);
-        fs.writeFileSync(filename, JSON.stringify(users_reg_data));
-
-        console.log(output_data)
-
-        response.send(`${username_input} registered!`);
-    }
-    else {
-        response.redirect('registration.html');
-    }
-
+    
 });
+
+ /*This is where the magic happens: We figure out what
+  - Cards belong to today, tomorrow, this week, etc.
+  - Organize by time
+        - The most essential is the date & the time
+            - Compare vs a function that records today vs date
+            - Compare times for all the arrays
+        - Example: Fetching all the same product types
+  - Organize by tag
+  - Have one for all */
+
+
+// Make an username input for all the pages for now. 
+
+console.log (users_reg_data.tester.tasks[0])
+console.log (users_reg_data.tester.tasks[0].title)
+console.log (users_reg_data.tester.tasks[0].date)
+console.log (users_reg_data.tester.tasks[0].time)
+
+
 
 
 // look for files in the "public" folder and listen on port 8080
